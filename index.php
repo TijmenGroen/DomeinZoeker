d5381dbfe5b6e0b59e564ee7a8425ffbc70f363b
Tijmen <tijmen.groen@outlook.com>
2025-01-27 12:20:34 +0100

    init

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="index.css">
+    <title>DomeinZoeker</title>
+</head>
+
+<body>
+    <div class="search-content">
+        <div class="search-header"></div>
+        <div class="search-body">
+            <div class="search-bar-container">
+                <form>
+                    <input type="text" name="search-bar" class="search-bar">
+                </form>
+            </div>
+            <div class="results-container">
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
