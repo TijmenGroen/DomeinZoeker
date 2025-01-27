b90b1b61d30fc048a171bdf2d5b09ae198600756
Tijmen <tijmen.groen@outlook.com>
2025-01-27 14:04:42 +0100

    added db connection

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,1 +1,17 @@
 <?php
+
+require_once realpath(__DIR__ . "/vendor/autoload.php");
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$dbhost = $_ENV["DB_HOST"];
+$dbname = $_ENV["DB_NAME"];
+
+$dsn = "mysql:host=$dbhost;dbname=$dbname";
+$dbusername = $_ENV["DB_USERNAME"];
+$dbpassword = $_ENV["DB_PASSWORD"];
+
+$pdo = new PDO($dsn, $dbusername, $dbpassword);
